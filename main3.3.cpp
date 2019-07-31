2c252761657d677996ac76239ac819c7b339085d
antonosmolovskyi <53257237+antonosmolovskyi@users.noreply.github.com>
2019-07-31 17:08:29 +0200

    Update main3.3.cpp

--- main3.3.cpp
+++ main3.3.cpp
@@ -3,12 +3,11 @@
 int main () {
   int num = 0;
   int temp = 0;
-  const int TWO = 0;
   std::cout << "Enter count: " << std::endl;
   std::cin >> num;
   while (num) {
-    num = num / TWO;
-    temp = num % TWO;
+    num = num / 2;
+    temp = num % 2;
     std::cout << temp ;
   }
   std::cout << num;

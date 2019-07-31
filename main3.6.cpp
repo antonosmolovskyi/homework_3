235b6d5eb97d92154769311a94cdd5063c1af358
antonosmolovskyi <53257237+antonosmolovskyi@users.noreply.github.com>
2019-07-31 17:10:37 +0200

    Update main3.6.cpp

--- main3.6.cpp
+++ main3.6.cpp
@@ -4,20 +4,15 @@
 int main () {
   int num = 0;
   int counter = 0;
-  const int TWO = 2;
-  const int ZERO = 0;
-  const int FIFTY = 50;
-  const int SIXSTY = 60;
-  const int NINETY = 90;
   std::cout<<"Please Enter numbers: " << std::endl;
   std::cout<<"/if you want to stop entering numbers, enter * " << std::endl;
-  for (int i = ZERO; i < FIFTY; ++i) {
+  for (int i = 0; i < 50; ++i) {
     std::cin >> num;
-    if ( num >= -SIXSTY && num <= NINETY) {
+    if ( num >= -60 && num <= 90) {
       if (num == '*') {
-        i = FIFTY;
+        i = 50;
       }
-      if (num % TWO != ZERO) {
+      if (num % 2 != 0) {
         counter += num;
       }
     } else {

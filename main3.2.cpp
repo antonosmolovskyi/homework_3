91a346cc17b49d345cee73f8c23617829deb9037
antonosmolovskyi <53257237+antonosmolovskyi@users.noreply.github.com>
2019-07-31 17:07:38 +0200

    Update main3.2.cpp

--- main3.2.cpp
+++ main3.2.cpp
@@ -3,22 +3,19 @@
 int main() {
   int valuer = 0;
   int valuer_number = 1;
-  const int TEN = 10;
-  const int ZERO = 0;
-  const int ONE = 1;
-	std::cin >> valuer;
-	int save_to_valuer = valuer;
-	int save_to_valuer1 = valuer;
-	while ((valuer/=TEN) > ZERO) valuer_number++;
-  int first_value = ONE;
-  int second_value = TEN;
-  int counter = ZERO;
-  float average = ZERO;
+  std::cin >> valuer;
+  int save_to_valuer = valuer;
+  int save_to_valuer1 = valuer;
+  while ((valuer/=10) > 0) valuer_number++;
+  int first_value = 1;
+  int second_value = 10;
+  int counter = 0;
+  float average = 0;
 
-  for (int i = ZERO; i < valuer_number; ++i) {
+  for (int i = 0; i < valuer_number; ++i) {
      counter += (save_to_valuer % second_value) / first_value;
-     second_value *= TEN;
-     first_value *= TEN;
+     second_value *= 10;
+     first_value *= 10;
      save_to_valuer = save_to_valuer1;
      average += i;
   }

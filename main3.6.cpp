2616a961d8ff40652ee579b8769aefc2b0990085
antonosmolovskyi <53257237+antonosmolovskyi@users.noreply.github.com>
2019-08-04 20:48:43 +0200

    Update main3.6.cpp

--- main3.6.cpp
+++ main3.6.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 
-
 int main () {
   int num = 0;
   int counter = 0;
-  std::cout<<"Please Enter numbers, to count the sum of the odd elements " << std::endl;
-  std::cout<<"/if you want to stop entering numbers, enter * " << std::endl;
+  std::cout<<"Please Enter numbers, to count the sum of the odd elements \n";
+  std::cout<<"/if you want to stop entering numbers, enter * \n";
   for (int i = 0; i < 50; ++i) {
-    std::cin >> num;
-    if ( num >= -60 && num <= 90) {
-      if (num == '*') {
-        i = 50;
-      }
-      if (num % 2 != 0) {
-        counter += num;
-      }
-    } else {
-        std::cout << "please enter correct number (-60 to 90)" << endl;
-        --i;
-      }
+  std::cin >> num;
+  if ( num >= -60 && num <= 90) {
+    if (num == '*') {
+      i = 50;
+    }    
+    if (num % 2 != 0) {
+      counter += num;
+    }
+  } else {
+      std::cout << "please enter correct number (-60 to 90) \n";
+      --i;
+    }
   }
-  std::cout << counter ;
+  std::cout << counter << '\n';
   return 0;
 }
 
